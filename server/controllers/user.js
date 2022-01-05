3e9b950a88d50e4c37d975e66c4486a76cdd0eae
indrakj <indrakomara13@gmail.com>
2022-01-05 16:51:32 +0700

    add crud

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,48 @@
 const UserModel = require('../models/user');
+const mongoose = require('mongoose');
 
 class UserController {
   static getUser (req,res) {
     UserModel.find({})
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+  };
+ 
+  static postUser (req,res) {
+    UserModel.create({
+      name: req.body.name,
+      age: req.body.age,
+      hobby: req.body.hobby 
+    })
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+  };
+
+  static putUser (req,res) {
+    UserModel.updateOne({_id : req.params.id}, {$set: {
+      name: req.body.name,
+      age: req.body.age,
+      hobby: req.body.hobby}
+    })
+    .then(data => {
+      res.status(200).send(data)
+    })
+    .catch(err => {
+      res.status(500).send(err)
+    })
+  };
+
+  static deleteUser (req,res) {
+    UserModel.deleteOne({_id : req.params.id
+    })
     .then(data => {
       res.status(200).send(data)
     })
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,4 +3,7 @@ const router = express.Router();
 const API = require('../controllers/user');
 
 router.get('/users', API.getUser);
+router.post('/new', API.postUser);
+router.put('/update/:id', API.putUser);
+router.delete('/delete/:id', API.deleteUser);
 module.exports = router;
